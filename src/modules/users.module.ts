5e7d0ca05854622572c84aba2c113381a38c08f3
Marcus Antonio <marcus@soganhei.com.br>
2026-02-20 20:20:45 -0300

    fix: remove import

--- src/modules/users.module.ts
+++ src/modules/users.module.ts
@@ -1,4 +1,3 @@
-import { FindUsersController } from "@controller/account/find-account.controller";
 import { CreateUsersController } from "@controller/users/create-users.controller";
 import { Module } from "@nestjs/common";
 import { UsersRepository } from "@repository/users.repository";
@@ -6,7 +5,7 @@ import { CreateUsersService } from "@service/users/create-users.service";
 
 @Module({
 	imports: [],
-	controllers: [CreateUsersController, FindUsersController],
+	controllers: [CreateUsersController],
 	providers: [CreateUsersService, UsersRepository],
 })
 export class UsersModule {}
